1913016533b44db3788509819522f315f6fec67c
fazilnbr <fazilkp2000@gmail.com>
2023-04-29 17:50:08 +0530

    inject dependency
    depency inject with using wire

--- SA01-grpc-order-svc/cmd/api/main.go
+++ SA01-grpc-order-svc/cmd/api/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+
+	"github.com/fazilnbr/SA01-OrderManagement/SA01-grpc-order-svc/pkg/config"
+	"github.com/fazilnbr/SA01-OrderManagement/SA01-grpc-order-svc/pkg/di"
+)
+
+func main() {
+	config, configErr := config.LoadConfig()
+	if configErr != nil {
+		log.Fatal("cannot load config: ", configErr)
+	}
+	fmt.Println("config : ", config)
+
+	server, diErr := di.InitializeAPI(config)
+	if diErr != nil {
+		log.Fatal("cannot start server: ", diErr)
+	} else {
+		server.Start()
+	}
+
 }
--- SA01-grpc-order-svc/pkg/api/services/orderHandler.go
+++ SA01-grpc-order-svc/pkg/api/services/orderHandler.go
@@ -1,1 +1,13 @@
-package handler
+package services
+
+import usecase "github.com/fazilnbr/SA01-OrderManagement/SA01-grpc-order-svc/pkg/usecase/interface"
+
+type OrderService struct {
+	orderUseCase usecase.OrderUseCase
+}
+
+func NewOrderService(usecase usecase.OrderUseCase) *OrderService {
+	return &OrderService{
+		orderUseCase: usecase,
+	}
+}
