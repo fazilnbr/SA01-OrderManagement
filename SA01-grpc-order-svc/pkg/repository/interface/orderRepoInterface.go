cf7cc66963bcb735ee66dcc01ea639afd0b587b5
fazilnbr <fazilkp2000@gmail.com>
2023-04-29 19:43:23 +0530

    add():add create order repo funtion

--- SA01-grpc-order-svc/pkg/repository/interface/orderRepoInterface.go
+++ SA01-grpc-order-svc/pkg/repository/interface/orderRepoInterface.go
@@ -1,4 +1,11 @@
 package interfaces
 
+import (
+	"context"
+
+	"github.com/fazilnbr/SA01-OrderManagement/SA01-grpc-order-svc/pkg/domain"
+)
+
 type OrderRepository interface {
+	CreateOrder(ctx context.Context, order domain.Order) (int, error)
 }
--- SA01-grpc-order-svc/pkg/repository/orderRepository.go
+++ SA01-grpc-order-svc/pkg/repository/orderRepository.go
@@ -1,12 +1,21 @@
 package repository
 
 import (
+	"context"
+
+	"github.com/fazilnbr/SA01-OrderManagement/SA01-grpc-order-svc/pkg/domain"
 	interfaces "github.com/fazilnbr/SA01-OrderManagement/SA01-grpc-order-svc/pkg/repository/interface"
 	"gorm.io/gorm"
 )
 
 type orderDatabase struct {
 	DB *gorm.DB
+}
+
+// CreateOrder implements interfaces.OrderRepository
+func (o *orderDatabase) CreateOrder(ctx context.Context, order domain.Order) (int, error) {
+	err := o.DB.Create(&order).Error
+	return order.Item_id, err
 }
 
 func NewOrderRepository(DB *gorm.DB) interfaces.OrderRepository {
