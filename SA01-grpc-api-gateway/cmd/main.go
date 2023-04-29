4c6a0a20738f8caf8e2e199f221b9cacc9864395
fazilnbr <fazilkp2000@gmail.com>
2023-04-29 18:11:27 +0530

    compleet initial stage proccess

--- SA01-grpc-api-gateway/cmd/main.go
+++ SA01-grpc-api-gateway/cmd/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main(){
+import (
+	"log"
+
+	"github.com/fazilnbr/SA01-OrderManagement/SA01-grpc-api-gateway/pkg/config"
+	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	swaggerfiles "github.com/swaggo/files"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	r := gin.Default()
+
+	// Swagger docs
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	r.Run(cfg.Port)
 
 }
--- SA01-grpc-api-gateway/pkg/order/client.go
+++ SA01-grpc-api-gateway/pkg/order/client.go
@@ -1,1 +1,24 @@
 package order
+
+import (
+	"fmt"
+
+	"github.com/fazilnbr/SA01-OrderManagement/SA01-grpc-api-gateway/pkg/config"
+	"github.com/fazilnbr/SA01-OrderManagement/SA01-grpc-api-gateway/pkg/order/pb"
+	"google.golang.org/grpc"
+)
+
+type ServiceClient struct {
+	Client pb.CartServiceClient
+}
+
+func InitServiceClient(c *config.Config) pb.CartServiceClient {
+	// using WithInsecure() because no SSL running
+	cc, err := grpc.Dial(c.OrderSvcUrl, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("Could not connect:", err)
+	}
+
+	return pb.NewCartServiceClient(cc)
+}
